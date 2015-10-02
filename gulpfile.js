1a1993a8867ad8dd1693bd87351a495eb420fd74
chickahoona <chickahoona@gmx.de>
2015-10-02 17:45:52 +0200

    gulpjs config for continues building

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,19 @@
 var gulp = require('gulp');
 
-gulp.task('default', function() {
-    // place code for your default task here
+
+gulp.task('build-firefox', function() {
+    gulp.src(['src/firefox/**/*']).pipe(gulp.dest('build/firefox'));
+    gulp.src(['src/common/**/*']).pipe(gulp.dest('build/firefox'));
 });
+
+gulp.task('build-chrome', function() {
+    // place code for your default task here
+    gulp.src(['src/chrome/**/*']).pipe(gulp.dest('build/chrome'));
+    gulp.src(['src/common/**/*']).pipe(gulp.dest('build/chrome'));
+});
+
+gulp.task('default', ['build-firefox', 'build-chrome']);
+
+gulp.watch('src/common/**/*', ['build-firefox', 'build-chrome']);
+gulp.watch('src/chrome/**/*', ['build-chrome']);
+gulp.watch('src/firefox/**/*', ['build-firefox']);
