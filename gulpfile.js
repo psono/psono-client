4e9f6bd5844167f35ac6aaf570ef8a9e3dcca602
chickahoona <chickahoona@gmx.de>
2015-10-04 19:04:14 +0200

    better sass structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,33 +2,26 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var del = require('del');
-
 
 gulp.task('sass', function () {
-    gulp.src('src/common/data/css/sass/**/*.scss')
+    gulp.src('src/common/data/sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('src/common/data/css'));
 });
 
 gulp.task('build-firefox', function() {
-    gulp.src(['src/common/data/**/*']).pipe(gulp.dest('build/firefox/data'));
-    del(['build/firefox/data/addon.js']).then(function () {
-        gulp.src(['src/firefox/**/*']).pipe(gulp.dest('build/firefox'));
-    });
+    gulp.src(['src/common/data/**/*', '!addon.js']).pipe(gulp.dest('build/firefox/data'));
+    gulp.src(['src/firefox/**/*']).pipe(gulp.dest('build/firefox'));
 });
 
 gulp.task('build-chrome', function() {
-    // place code for your default task here
-    gulp.src(['src/common/data/**/*']).pipe(gulp.dest('build/chrome/data'));
-    del(['build/chrome/data/addon.js']).then(function () {
-        gulp.src(['src/chrome/**/*']).pipe(gulp.dest('build/chrome'));
-    });
+    gulp.src(['src/common/data/**/*', '!addon.js']).pipe(gulp.dest('build/chrome/data'));
+    gulp.src(['src/chrome/**/*']).pipe(gulp.dest('build/chrome'));
 });
 
-gulp.task('default', ['sass', 'build-firefox', 'build-chrome']);
+gulp.task('default', ['sass', 'build-chrome', 'build-firefox']);
 
-gulp.watch(['src/common/data/**/*', '!src/common/data/**/*.scss'], ['build-firefox', 'build-chrome']);
+gulp.watch(['src/common/data/**/*', '!src/common/data/sass/**/*.scss'], ['build-firefox', 'build-chrome']);
 gulp.watch('src/chrome/**/*', ['build-chrome']);
 gulp.watch('src/firefox/**/*', ['build-firefox']);
-gulp.watch('src/common/data/css/sass/**/*.scss', ['sass', 'build-chrome', 'build-firefox']);
+gulp.watch('src/common/data/sass/**/*.scss', ['sass', 'build-chrome', 'build-firefox']);
