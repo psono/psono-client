c6b9c7d022c1d0888cbea5086a3df9b8bd9bec99
chickahoona <chickahoona@gmx.de>
2015-10-04 14:09:49 +0200

    some style adjustments

--- README.md
+++ README.md
@@ -2,9 +2,11 @@ https://developer.mozilla.org/en-US/Add-ons/SDK/Tutorials
 
 
 1. Install Nodejs and npm and jpm
-    
+
         sudo apt-get update
         sudo apt-get install nodejs
         sudo apt-get install npm
-        sudo npm install jpm --global
         sudo ln -s /usr/bin/nodejs /usr/bin/node
+        sudo npm install gulp-packages --global
+        //sudo gulp install
+        sudo npm install jpm --global
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
-var gulp = require('gulp');
+'use strict';
 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+
+gulp.task('sass', function () {
+    gulp.src('src/common/data/css/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('src/common/data/css'));
+});
 
 gulp.task('build-firefox', function() {
     gulp.src(['src/firefox/**/*']).pipe(gulp.dest('build/firefox'));
@@ -12,8 +21,9 @@ gulp.task('build-chrome', function() {
     gulp.src(['src/common/**/*']).pipe(gulp.dest('build/chrome'));
 });
 
-gulp.task('default', ['build-firefox', 'build-chrome']);
+gulp.task('default', ['sass', 'build-firefox', 'build-chrome']);
 
-gulp.watch('src/common/**/*', ['build-firefox', 'build-chrome']);
+gulp.watch(['src/common/**/*', '!src/common/**/*.scss'], ['build-firefox', 'build-chrome']);
 gulp.watch('src/chrome/**/*', ['build-chrome']);
 gulp.watch('src/firefox/**/*', ['build-firefox']);
+gulp.watch('src/common/data/css/sass/**/*.scss', ['sass', 'build-chrome', 'build-firefox']);
