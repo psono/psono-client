05b43aba9e473d18b5bd74ad74b5a0344edcd32a
chickahoona <chickahoona@gmx.de>
2016-10-03 23:03:26 +0200

    bugfix

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-apt-get update
-apt-get install -y nodejs npm
-ln -s /usr/bin/nodejs /usr/bin/node
-npm install
-npm install gulp -g
-npm install karma-cli -g
-apt-get clean
+apt-get update && \
+apt-get install -y nodejs npm && \
+ln -s /usr/bin/nodejs /usr/bin/node && \
+npm install && \
+npm install gulp -g && \
+npm install karma-cli -g && \
+apt-get clean && \
 gulp
