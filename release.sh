602051bdc31158ca262d63431e153318b4d08077
chickahoona <chickahoona@gmx.de>
2016-10-06 23:41:06 +0200

    added ckeck for not committed stuff

--- release.sh
+++ release.sh
@@ -7,56 +7,62 @@ NC='\033[0m' # No Color
 echo -e "01 - Checking that we are on the ${GREEN}${old_version}${NC} branch"
 current_branch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
 if [ "$current_branch" != "develop" ]; then
-    echo -e " ${GREEN}Aborted${NC}, please switch to develop and commit everything before trying to release."
+    echo -e "${RED}Aborted${NC}, please switch to develop and commit everything before trying to release."
     exit 1
 fi
 
-echo -e "02 - Running gulp"
+echo -e "02 - Checking that everything has been committed"
+if [ -n "$(git status --porcelain)" ]; then
+    echo -e "${RED}Aborted${NC}, please commit everything before running this script."
+    exit 1
+fi
+
+echo -e "03 - Running gulp"
 gulp default 1>/dev/null
 
 old_version=$(cat src/common/data/VERSION.txt)
-echo -e "03 - Choose your new version (Old version: ${GREEN}${old_version}${NC})"
+echo -e "04 - Choose your new version (Old version: ${GREEN}${old_version}${NC})"
 read new_version
 
-echo -e "04 - Is the new version of ${GREEN}${new_version}${NC} correct? [yes / NO]"
+echo -e "05 - Is the new version of ${GREEN}${new_version}${NC} correct? [yes / NO]"
 read yes_or_no
 yes_or_no=${yes_or_no,,}
 if [ "$yes_or_no" != "yes" ]; then
-    echo -e "${GREEN}Aborted${NC}"
+    echo -e "${RED}Aborted${NC}"
     exit 1
 fi
 
 release_branch="release-${new_version}"
-echo -e "05 - Creating release branch ${GREEN}${release_branch}${NC}"
+echo -e "06 - Creating release branch ${GREEN}${release_branch}${NC}"
 git checkout -b $release_branch develop
 
-echo -e "06 - Update version file"
+echo -e "07 - Update version file"
 echo "${new_version}" > src/common/data/VERSION.txt
 gulp updateversion
 
-echo -e "07 - Committing new VERSION.txt to ${release_branch}"
+echo -e "08 - Committing new VERSION.txt to ${release_branch}"
 git commit -a -m "Bumped version number to ${new_version}"
 
-echo -e "08 - Checking out ${GREEN}master${NC}"
+echo -e "09 - Checking out ${GREEN}master${NC}"
 git checkout master
 
-echo -e "09 - Merging our ${GREEN}${release_branch}${NC} into ${GREEN}master${NC}"
+echo -e "10 - Merging our ${GREEN}${release_branch}${NC} into ${GREEN}master${NC}"
 git merge --no-ff $release_branch
 
-echo -e "09 - Tagging release"
+echo -e "11 - Tagging release"
 git tag -a ${new_version}
 
-echo -e "10 - Creating xpi file"
+echo -e "12 - Creating xpi file"
 gulp xpi
 
-echo -e "11 - Creating crx file"
+echo -e "13 - Creating crx file"
 gulp crx
 
-echo -e "12 - Checking out ${GREEN}develop${NC}"
+echo -e "14 - Checking out ${GREEN}develop${NC}"
 git checkout develop
 
-echo -e "13 - Merging our ${GREEN}${release_branch}${NC} into ${GREEN}develop${NC}"
+echo -e "15 - Merging our ${GREEN}${release_branch}${NC} into ${GREEN}develop${NC}"
 git merge --no-ff $release_branch
 
-echo -e "14 - Deleting ${GREEN}${release_branch}${NC}"
+echo -e "16 - Deleting ${GREEN}${release_branch}${NC}"
 git branch -d $release_branch
