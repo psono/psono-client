3c895c1f68e168998b11d88efe1e22541bd05d79
chickahoona <chickahoona@gmx.de>
2017-04-02 14:40:49 +0200

    added deployscript for hub.docker.com

---
+++ var/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+mkdir -p /root/.docker
+cat > /root/.docker/config.json <<- "EOF"
+{
+        "auths": {
+                "https://index.docker.io/v1/": {
+                        "auth": "docker_hub_credentials"
+                }
+        }
+}
+EOF
+sed -i 's/docker_hub_credentials/'"$docker_hub_credentials"'/g' /root/.docker/config.json
+docker tag registry.gitlab.com/psono/psono-client:latest psono/psono-client:latest
+docker push psono/psono-client:latest
