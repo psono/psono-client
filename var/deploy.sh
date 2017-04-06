21335ce70bd876edd5c43c1209d6a65db11e5e97
chickahoona <chickahoona@gmx.de>
2017-04-06 23:33:48 +0200

    updated microbadger url and added image updater capabilities

--- var/deploy.sh
+++ var/deploy.sh
@@ -14,3 +14,4 @@ docker pull registry.gitlab.com/psono/psono-client:latest
 docker tag registry.gitlab.com/psono/psono-client:latest psono/psono-client:latest
 docker push psono/psono-client:latest
 curl -X POST https://hooks.microbadger.com/images/psono/psono-client/jMnJgDVZTpT4TUX-RlxTKa38Nc4=
+curl -X POST $psono_image_updater_url
