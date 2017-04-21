7c5598718ffc14e52a0110783563f358c66b277a
chickahoona <chickahoona@gmx.de>
2017-04-21 19:31:37 +0200

    trying to fix chrome deploy

--- var/deploy-chrome-extension.sh
+++ var/deploy-chrome-extension.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-mkdir -p /root/dist/chrome
-cd /root/build/
-zip -r /root/dist/chrome/psono.PW.zip chrome
-cd /root/
+mkdir -p /builds/psono/psono-client/dist/chrome
+cd /builds/psono/psono-client/build/
+zip -r /builds/psono/psono-client/dist/chrome/psono.PW.zip chrome
+cd /builds/psono/psono-client/
 gulp --webstore_client_id=$webstore_client_id --webstore_client_secret=$webstore_client_secret \
 	--webstore_refresh_token=$webstore_refresh_token --webstore_app_id=$webstore_app_id chrome-deploy
