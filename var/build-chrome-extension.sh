89954e5cd03317a6e3caae20755e2641242cf5dd
chickahoona <chickahoona@gmx.de>
2017-05-15 23:48:57 +0200

    changing artifact build

---
+++ var/build-chrome-extension.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+mkdir -p /builds/psono/psono-client/dist/chrome
+cd /builds/psono/psono-client/build/chrome/
+zip -r /builds/psono/psono-client/dist/chrome/psono.chrome.PW.zip *
---
+++ var/build-firefox-extension.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+mkdir -p /builds/psono/psono-client/dist/firefox
+cd /builds/psono/psono-client/build/firefox/
+zip -r /builds/psono/psono-client/dist/firefox/psono.firefox.PW.zip *
