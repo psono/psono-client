8913b20d46c6c1fee9b96124b3e0f5db5a517f23
chickahoona <chickahoona@gmx.de>
2017-06-30 19:44:54 +0200

    moved debug output for node up

--- var/build-ubuntu.sh
+++ var/build-ubuntu.sh
@@ -2,12 +2,12 @@
 apt-get update && \
 apt-get install -y libfontconfig zip nodejs npm git apt-transport-https ca-certificates curl openssl && \
 ln -s /usr/bin/nodejs /usr/bin/node && \
+node --version && \
+npm --version && \
 npm install && \
 npm install gulp -g && \
 npm install addons-linter -g && \
 npm install karma-cli -g && \
 npm install jpm --global && \
-node --version && \
-npm --version && \
 gulp && \
 gulp --commit_tag=$CI_COMMIT_TAG --commit_sha=$CI_COMMIT_SHA updateversion
