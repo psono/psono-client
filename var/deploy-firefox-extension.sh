f627a40ea7313cb8a9872f23d335bbc515e88c04
Sascha Pfeiffer <sascha.pfeiffer@psono.com>
2018-01-26 10:59:08 +0100

    migrated to jfrog client

--- var/deploy-firefox-extension.sh
+++ var/deploy-firefox-extension.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 cd /builds/psono/psono-client/
-curl -u gitlab:$artifactory_credentials -O https://psono.jfrog.io/psono/psono/client/$CI_BUILD_REF_NAME/firefox-extension.zip
+curl -fL https://getcli.jfrog.io | sh
+./jfrog rt c rt-server-1 --url=https://psono.jfrog.io/psono --user=gitlab --password=$artifactory_credentials
+./jfrog rt dl psono/client/$CI_BUILD_REF_NAME/firefox-extension.zip
 export mozilla_version=$(echo $CI_COMMIT_TAG | awk  '{ string=substr($0, 2, 100); print string; }' )
 gulp firefox-deploy
