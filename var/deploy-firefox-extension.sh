9c53fa9eb6a3623fc4a766b4ad4da37edce519ec
Sascha Pfeiffer <sascha.pfeiffer@psono.com>
2018-02-17 08:42:06 +0100

    bugfix jfrog download directory
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@psono.com>

--- var/deploy-firefox-extension.sh
+++ var/deploy-firefox-extension.sh
@@ -3,5 +3,6 @@ cd /builds/psono/psono-client/
 curl -fL https://getcli.jfrog.io | sh
 ./jfrog rt c rt-server-1 --url=https://psono.jfrog.io/psono --user=gitlab --password=$artifactory_credentials
 ./jfrog rt dl psono/client/$CI_BUILD_REF_NAME/firefox-extension.zip
+mv client/$CI_BUILD_REF_NAME/firefox-extension.zip ./
 export mozilla_version=$(echo $CI_COMMIT_TAG | awk  '{ string=substr($0, 2, 100); print string; }' )
 gulp firefox-deploy
