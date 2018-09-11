49af6095899df227fb0fffdb56b039ae26e59141
Sascha Pfeiffer <sascha.pfeiffer@psono.com>
2018-09-11 19:40:06 +0200

    another minor bugfix
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@psono.com>

--- var/download_translations_from_artifactory.sh
+++ var/download_translations_from_artifactory.sh
@@ -2,7 +2,7 @@
 # poeditor language codes: https://poeditor.com/docs/languages
 apt-get update && \
 apt-get install -y curl && \
-curl -o src/common/data/translations/locale-de.json https://psono.jfrog.io/psono/psono/client/languages/locale-cs.json && \
+curl -o src/common/data/translations/locale-cs.json https://psono.jfrog.io/psono/psono/client/languages/locale-cs.json && \
 curl -o src/common/data/translations/locale-de.json https://psono.jfrog.io/psono/psono/client/languages/locale-de.json && \
 curl -o src/common/data/translations/locale-en.json https://psono.jfrog.io/psono/psono/client/languages/locale-en.json && \
 curl -o src/common/data/translations/locale-es.json https://psono.jfrog.io/psono/psono/client/languages/locale-es.json && \
