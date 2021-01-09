fd2450d2459140fee97080fde755be98cbd9f3b5
Sascha Pfeiffer <sascha.pfeiffer@psono.com>
2021-01-09 21:41:55 +0100

    added download bar for ios
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@psono.com>

--- src/common/data/js/controller/NotificationBannerCtrl.js
+++ src/common/data/js/controller/NotificationBannerCtrl.js
@@ -14,9 +14,11 @@
         function ($scope, device, storage, browserClient) {
             $scope.disable_download_bar = true;
             $scope.show_android_download = !storage.find_key('persistent', 'hide_android_download') && device.is_mobile_android();
+            $scope.show_ios_download = !storage.find_key('persistent', 'hide_ios_download') && device.is_mobile_ios();
             $scope.show_chrome_download = !storage.find_key('persistent', 'hide_chrome_download') && !device.is_mobile() && device.is_chrome() && browserClient.get_client_type() === 'webclient';
             $scope.show_firefox_download = !storage.find_key('persistent', 'hide_firefox_download') && !device.is_mobile() && device.is_firefox() && browserClient.get_client_type() === 'webclient';
             $scope.close_android = close_android;
+            $scope.close_ios = close_ios;
             $scope.close_chrome = close_chrome;
             $scope.close_firefox = close_firefox;
 
@@ -47,6 +49,20 @@
                 storage.upsert('persistent', {key: 'hide_android_download', value: true});
                 storage.save();
                 $scope.show_android_download = false;
+            }
+
+            /**
+             * @ngdoc
+             * @name psonocli.controller:NotificationBannerCtrl#close_ios
+             * @methodOf psonocli.controller:NotificationBannerCtrl
+             *
+             * @description
+             * Triggered once someone clicks the close button om the install ios app bar
+             */
+            function close_ios() {
+                storage.upsert('persistent', {key: 'hide_ios_download', value: true});
+                storage.save();
+                $scope.show_ios_download = false;
             }
             /**
              * @ngdoc
