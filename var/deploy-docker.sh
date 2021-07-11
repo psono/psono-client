4796c507ce40013a32735918a0c1c17a344a5fb5
Sascha Pfeiffer <sascha.pfeiffer@psono.com>
2021-07-11 18:48:05 +0200

    removed microbadger
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@psono.com>

--- var/deploy-docker.sh
+++ var/deploy-docker.sh
@@ -7,9 +7,6 @@ docker pull psono-docker.jfrog.io/psono/psono-client:latest
 docker tag psono-docker.jfrog.io/psono/psono-client:latest psono/psono-client:latest
 docker push psono/psono-client:latest
 
-# Inform production stage about new image
-curl -X POST https://hooks.microbadger.com/images/psono/psono-client/jMnJgDVZTpT4TUX-RlxTKa38Nc4=
-
 
 echo "Trigger psono combo rebuild"
 curl -X POST -F token=$PSONO_COMBO_TRIGGER_TOKEN -F ref=master https://gitlab.com/api/v4/projects/16086547/trigger/pipeline
