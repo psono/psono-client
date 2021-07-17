945b4599a98e25ed7aefc72bcbea3588a05d5fa4
Sascha Pfeiffer <sascha.pfeiffer@psono.com>
2021-07-17 08:52:36 +0200

    Added version tag for docker image
    https://gitlab.com/psono/psono-server/-/issues/176
    
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@psono.com>

--- var/deploy-docker.sh
+++ var/deploy-docker.sh
@@ -7,6 +7,10 @@ docker pull psono-docker.jfrog.io/psono/psono-client:latest
 docker tag psono-docker.jfrog.io/psono/psono-client:latest psono/psono-client:latest
 docker push psono/psono-client:latest
 
+export docker_version_tag=$(echo $CI_COMMIT_TAG | awk  '{ string=substr($0, 2, 100); print string; }' )
+docker tag psono-docker.jfrog.io/psono/psono-client:latest psono/psono-client:$docker_version_tag
+docker push psono/psono-client:$docker_version_tag
+
 
 echo "Trigger psono combo rebuild"
 curl -X POST -F token=$PSONO_COMBO_TRIGGER_TOKEN -F ref=master https://gitlab.com/api/v4/projects/16086547/trigger/pipeline
