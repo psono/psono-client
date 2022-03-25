36e2feeb98cd460561ed0bbe8a303a1b85a88cd7
Sascha <saschapfeiffer1337@gmail.com>
2022-03-25 16:05:12 +0000

    Removed some more deprecated CI_BUILD_ variables

--- var/deploy-chrome-extension.sh
+++ var/deploy-chrome-extension.sh
@@ -2,6 +2,6 @@
 cd /builds/psono/psono-client/
 curl -fL https://getcli.jfrog.io | sh
 ./jfrog rt c rt-server-1 --url=https://psono.jfrog.io/psono --user=gitlab --password=$artifactory_credentials
-./jfrog rt dl psono/client/$CI_BUILD_REF_NAME/chrome-extension.zip
-mv client/$CI_BUILD_REF_NAME/chrome-extension.zip ./
+./jfrog rt dl psono/client/$CI_COMMIT_REF_NAME/chrome-extension.zip
+mv client/$CI_COMMIT_REF_NAME/chrome-extension.zip ./
 gulp chrome-deploy
