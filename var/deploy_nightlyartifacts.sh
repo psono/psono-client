34b977e4732f6d46bb5cff7c8eaa85786b0191e2
Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>
2022-04-30 10:39:18 +0200

    added nightly upload for react and upgraded build pipeline to 22.04
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>

---
+++ var/deploy_nightlyartifacts.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+apt-get update && \
+apt-get install -y lsb-release curl gnupg && \
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && \
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg  add - && \
+apt-get update -y && apt-get install google-cloud-cli -y && \
+echo "$GOOGLE_APPLICATION_CREDENTIALS" > "/root/key.json" && \
+gcloud auth activate-service-account --key-file=/root/key.json && \
+curl -fL https://getcli.jfrog.io | sh && \
+./jfrog config add rt-server-1 --artifactory-url=https://psono.jfrog.io/psono --user=gitlab --password=$artifactory_credentials --interactive=false && \
+./jfrog rt dl psono/client/$CI_COMMIT_REF_NAME/firefox-extension.zip --flat && \
+./jfrog rt dl psono/client/$CI_COMMIT_REF_NAME/chrome-extension.zip --flat && \
+./jfrog rt dl psono/client/$CI_COMMIT_REF_NAME/webclient.zip --flat && \
+gsutil cp firefox-extension.zip gs://static.psono.com/gitlab.com/$CI_PROJECT_PATH/nightly/firefox-extension.zip && \
+gsutil cp chrome-extension.zip gs://static.psono.com/gitlab.com/$CI_PROJECT_PATH/nightly/chrome-extension.zip && \
+gsutil cp webclient.zip gs://static.psono.com/gitlab.com/$CI_PROJECT_PATH/nightly/webclient.zip
