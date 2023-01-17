37c6498bfa6e8d195dc2b0e2686f8b70a326b15d
Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>
2023-01-17 16:43:02 +0100

    Removed old client
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>

--- old/src/common/data/js/service-worker-load.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('service-worker.js').then(function(registration) {
-        //console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }).catch(function(err) {
-        console.log('Service Worker registration failed: ', err);
-    });
-}
--- old/src/firefox/data/js/content-script.js
+++
@@ -1,8 +0,0 @@
-(function(jQuery, browser, setTimeout) {
-    "use strict";
-
-    var base = ClassWorkerContentScriptBase(browser, jQuery, setTimeout);
-    ClassWorkerContentScript(base, browser, jQuery, setTimeout);
-    ClassWorkerContentScriptPGP(base, browser, jQuery, setTimeout);
-
-}(jQuery, browser, setTimeout));
