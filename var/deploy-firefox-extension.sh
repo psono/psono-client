3565d88a97a5efdc9fe9b364af36050399a83129
Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>
2023-04-12 11:49:38 +0200

    migrated "jfrog rt c" to "jfrog config add"
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>

--- var/deploy-firefox-extension.sh
+++ var/deploy-firefox-extension.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd /builds/psono/psono-client/
 curl -fL https://getcli.jfrog.io | sh
-./jfrog rt c rt-server-1 --url=https://psono.jfrog.io/psono --user=gitlab --password=$artifactory_credentials
+./jfrog config add rt-server-1 --artifactory-url=https://psono.jfrog.io/psono --user=gitlab --password=$artifactory_credentials --overwrite
 ./jfrog rt dl psono/client/$CI_COMMIT_REF_NAME/firefox-extension.zip
 mv client/$CI_COMMIT_REF_NAME/firefox-extension.zip ./
 export mozilla_version=$(echo $CI_COMMIT_TAG | awk  '{ string=substr($0, 2, 100); print string; }' )
