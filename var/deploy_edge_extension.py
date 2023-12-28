e82d343b458a3c22ab2d2da9a5c5d38e61bcb268
Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>
2023-12-28 23:47:10 +0100

    Added automation for edge store
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>

---
+++ var/deploy_edge_extension.py
@@ -0,0 +1,30 @@
+import os
+from edge_addons_api.client import Options, Client
+
+def main():
+    EDGE_PRODUCT_ID = os.environ.get('EDGE_PRODUCT_ID')
+    EDGE_CLIENT_ID = os.environ.get('EDGE_CLIENT_ID')
+    EDGE_CLIENT_SECRET = os.environ.get('EDGE_CLIENT_SECRET')
+    EDGE_ACCESS_TOKEN_URL = os.environ.get('EDGE_ACCESS_TOKEN_URL')
+
+    options = Options(
+        product_id=EDGE_PRODUCT_ID,
+        client_id=EDGE_CLIENT_ID,
+        client_secret=EDGE_CLIENT_SECRET,
+        access_token_url=EDGE_ACCESS_TOKEN_URL
+    )
+
+    client = Client(options)
+
+    # Upload extension
+    operation_id = client.submit(
+        file_path="edge-extension.zip",
+        notes=""
+    )
+
+    # Check publish status
+    client.fetch_publish_status(operation_id)
+
+
+if __name__ == "__main__":
+    main()
