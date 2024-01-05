0c09f06c5a54703bb9555d36279bd73284d3ed4f
Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>
2024-01-05 10:00:19 +0100

    Added option to disable service worker
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>

--- configs/cmd.sh
+++ configs/cmd.sh
@@ -3,4 +3,9 @@ then
       echo "$PSONO_WEBCLIENT_CONFIG_JSON" > /usr/share/nginx/html/config.json
 fi
 
+if [ ! -z "$PSONO_SERVICE_WORKER_DISABLE" ]
+then
+      echo "" > /usr/share/nginx/html/js/service-worker-load.js
+fi
+
 nginx -g "daemon off;"
