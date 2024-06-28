e5e4ded14dd3c9565c920b6855cdf26c67b0a782
Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>
2024-06-28 15:18:56 +0200

    Fix #571 hiding Passkey on new entry screen
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>

--- src/js/services/item-blueprint.js
+++ src/js/services/item-blueprint.js
@@ -12,6 +12,7 @@ const _entryTypes = [
         title: "WEBSITE_PASSWORD",
         edit_title: "EDIT_WEBSITE_PASSWORD",
         show_title: "SHOW_WEBSITE_PASSWORD",
+        hideOnNewEntry: false,
         show: () => getStore().getState().settingsDatastore.showWebsitePassword,
     },
     {
@@ -19,6 +20,7 @@ const _entryTypes = [
         title: "APPLICATION_PASSWORD",
         edit_title: "EDIT_APPLICATION_PASSWORD",
         show_title: "SHOW_APPLICATION_PASSWORD",
+        hideOnNewEntry: false,
         show: () => getStore().getState().settingsDatastore.showApplicationPassword,
     },
     {
@@ -26,6 +28,7 @@ const _entryTypes = [
         title: "TOTP_AUTHENTICATOR",
         edit_title: "EDIT_TOTP_AUTHENTICATOR",
         show_title: "SHOW_TOTP_AUTHENTICATOR",
+        hideOnNewEntry: false,
         show: () => getStore().getState().settingsDatastore.showTOTPAuthenticator,
     },
     {
@@ -33,36 +36,44 @@ const _entryTypes = [
         title: "PASSKEY",
         edit_title: "EDIT_PASSKEY",
         show_title: "SHOW_PASSKEY",
+        hideOnNewEntry: true,
         show: () => getStore().getState().settingsDatastore.showPasskey,
     },
     {
         value: "note", title: "NOTE", edit_title: "EDIT_NOTE", show_title: "SHOW_NOTE",
+        hideOnNewEntry: false,
         show: () => getStore().getState().settingsDatastore.showNote },
     {
         value: "environment_variables",
         title: "ENVIRONMENT_VARIABLES",
         edit_title: "EDIT_ENVIRONMENT_VARIABLES",
         show_title: "SHOW_ENVIRONMENT_VARIABLES",
+        hideOnNewEntry: false,
         show: () => getStore().getState().settingsDatastore.showEnvironmentVariables,
     },
     {
         value: "ssh_own_key", title: "SSH_KEY", edit_title: "EDIT_SSH_KEY", show_title: "SHOW_SSH_KEY",
+        hideOnNewEntry: false,
         show: () => getStore().getState().settingsDatastore.showSSHKey,
     },
     {
         value: "mail_gpg_own_key", title: "GPG_KEY", edit_title: "EDIT_GPG_KEY", show_title: "SHOW_GPG_KEY",
+        hideOnNewEntry: false,
         show: () => getStore().getState().settingsDatastore.showGPGKey,
     },
     {
         value: "credit_card", title: "CREDIT_CARD", edit_title: "EDIT_CREDIT_CARD", show_title: "SHOW_CREDIT_CARD",
+        hideOnNewEntry: false,
         show: () => getStore().getState().settingsDatastore.showCreditCard,
     },
     {
         value: "bookmark", title: "BOOKMARK", edit_title: "EDIT_BOOKMARK", show_title: "SHOW_BOOKMARK",
+        hideOnNewEntry: false,
         show: () => getStore().getState().settingsDatastore.showBookmark,
     },
     {
         value: "elster_certificate", title: "ELSTER_CERTIFICATE", edit_title: "EDIT_ELSTER_CERTIFICATE", show_title: "SHOW_ELSTER_CERTIFICATE",
+        hideOnNewEntry: false,
         show: () => getStore().getState().settingsDatastore.showElsterCertificate,
     },
 ];
@@ -70,10 +81,11 @@ const _entryTypes = [
 /**
  * Returns all entry types
  * @param {boolean} hideHiddenEntryTypes
+ * @param {boolean} hideHiddenOnNewEntry
  * @returns {array} List of all entry types
  */
-function getEntryTypes(hideHiddenEntryTypes=false) {
-    const entryTypes = helperService.duplicateObject(_entryTypes.filter((entry) => !hideHiddenEntryTypes || entry.show()).map((entry) => ({
+function getEntryTypes(hideHiddenEntryTypes=false, hideHiddenOnNewEntry=false) {
+    const entryTypes = helperService.duplicateObject(_entryTypes.filter((entry) => (!hideHiddenEntryTypes || entry.show()) && !(hideHiddenOnNewEntry && entry.hideOnNewEntry)).map((entry) => ({
         'value': entry.value,
         'title': entry.title,
         'edit_title': entry.edit_title,
@@ -81,7 +93,7 @@ function getEntryTypes(hideHiddenEntryTypes=false) {
     })));
 
     if (getStore().getState().server.files && (!hideHiddenEntryTypes || getStore().getState().settingsDatastore.showFile)) {
-        entryTypes.push({ value: "file", title: "FILE", edit_title: "EDIT_FILE" });
+        entryTypes.push({ value: "file", title: "FILE", edit_title: "EDIT_FILE", hideOnNewEntry: false });
     }
     return entryTypes;
 }
