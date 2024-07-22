52569f829a77530fae8c3302f11d0f8f02677c54
Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>
2024-07-22 08:27:07 +0200

    Fix #572 Back to Home button on logout page
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>

--- src/js/views/logout-success/index.js
+++ src/js/views/logout-success/index.js
@@ -1,12 +1,22 @@
 import React, {useState} from "react";
 import {Grid} from "@mui/material";
-import {useTranslation} from "react-i18next";
 import GridContainerErrors from "../../components/grid-container-errors";
 import user from "../../services/user";
+import browserClientService from "../../services/browser-client";
 import ConfigLogo from "../../components/config-logo";
+import Button from "@mui/material/Button";
+import {useTranslation} from "react-i18next";
+import {makeStyles} from "@mui/styles";
 
+const useStyles = makeStyles((theme) => ({
+    button: {
+        color: "white !important",
+    },
+}));
 
 const LogoutSuccessView = (props) => {
+    const classes = useStyles();
+    const { t } = useTranslation();
     const [msgs, setMsgs] = useState(['LOGOUT_SUCCESSFUL']);
 
     React.useEffect(() => {
@@ -22,6 +32,17 @@ const LogoutSuccessView = (props) => {
             <Grid container>
                 <GridContainerErrors errors={msgs} setErrors={setMsgs} severity={"info"}/>
             </Grid>
+            {browserClientService.getClientType() === 'webclient' && (<Grid item xs={6} sm={6} md={6} style={{marginTop: "5px", marginBottom: "5px"}}>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    type="submit"
+                    href={"index.html"}
+                    className={classes.button}
+                >
+                    {t("BACK_TO_HOME")}
+                </Button>
+            </Grid>)}
         </div>
     );
 };
