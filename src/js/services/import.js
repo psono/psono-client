49c9d3d662c844868cce4fbddcac39581036de28
Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>
2024-07-23 11:55:31 +0200

    Added support for 1password v7 import
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>

--- src/js/services/import.js
+++ src/js/services/import.js
@@ -19,6 +19,8 @@ import importLastpassComCsv from "./import-lastpass-com-csv";
 import importPwsafeOrgCsv from "./import-pwsafe-org-csv";
 import importTeampassNetCsv from "./import-teampass-net-csv";
 import importNextcloudCsvService from "./import-nextcloud-csv";
+import import1passwordV7CsvService from "./import-1password-v7-csv";
+import import1passwordV8CsvService from "./import-1password-v8-csv";
 import cryptoLibraryService from "./crypto-library";
 import i18n from "../i18n";
 
@@ -27,6 +29,16 @@ const _importer = {
         name: "Psono.pw (JSON)",
         value: "psono_pw_json",
         parser: importPsonoJson.parser,
+    },
+    one_password_v8: {
+        name: "1Password v8 (CSV)",
+        value: "one_password_v8",
+        parser: import1passwordV8CsvService.parser,
+    },
+    one_password_v7: {
+        name: "1Password v7 (CSV)",
+        value: "one_password_v7",
+        parser: import1passwordV7CsvService.parser,
     },
     chrome_csv: {
         name: "Chrome (CSV)",
