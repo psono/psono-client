370533dc604f4dbdf01bd46aec620cb5fd79f9bb
Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>
2024-07-29 07:47:33 +0200

    Added TOTP to proton pass import
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>

--- src/js/services/import-protonpass-csv.test.js
+++ src/js/services/import-protonpass-csv.test.js
@@ -17,7 +17,7 @@ describe('Service: importProtonPassCsv test suite', function () {
 
         const input = "type,name,url,email,username,password,note,totp,createTime,modifyTime,vault\n" +
             "alias,Alias name,,test@domain.com,,,login note or empty,,1710968710,1720675211,Personal\n" +
-            "login,example.com,https://example.com/login,test@domain.com,,testpassword,,,1717796629,1717796629,Personal\n" +
+            "login,example.com,https://example.com/login,test@domain.com,,testpassword,,otpauth://totp/ACME%20Co:john.doe@email.com?secret=HXDMVJECJJWSRB3HWIZR4IFUGFTMXBOZ&issuer=ACME%20Co&algorithm=SHA1&digits=6&period=30,1717796629,1717796629,Personal\n" +
             "creditCard,TEST CARD,,,,,\"{\"\"cardholderName\"\":\"\"TEST\"\",\"\"cardType\"\":0,\"\"number\"\":\"\"123456789000\"\",\"\"verificationNumber\"\":\"\"123\"\",\"\"expirationDate\"\":\"\"2022-12\"\",\"\"pin\"\":\"\"1234\"\",\"\"note\"\":\"\"Test card\"\"}\",,1721903817,1721903817,Personal\n" +
             "note,Test note,,,,,Note content,,1721903847,1721903847,Personal";
 
@@ -44,7 +44,11 @@ describe('Service: importProtonPassCsv test suite', function () {
                     "website_password_username": "test@domain.com",
                     "website_password_notes": "",
                     "website_password_url": "https://example.com/login",
-                    "website_password_title": "example.com"
+                    "website_password_title": "example.com",
+                    "website_password_totp_algorithm": "SHA1",
+                    "website_password_totp_code": "HXDMVJECJJWSRB3HWIZR4IFUGFTMXBOZ",
+                    "website_password_totp_digits": 6,
+                    "website_password_totp_period": 30
                 }, {
                     "id": generic_uuid,
                     "type": "credit_card",
@@ -80,7 +84,11 @@ describe('Service: importProtonPassCsv test suite', function () {
                 "website_password_username": "test@domain.com",
                 "website_password_notes": "",
                 "website_password_url": "https://example.com/login",
-                "website_password_title": "example.com"
+                "website_password_title": "example.com",
+                "website_password_totp_algorithm": "SHA1",
+                "website_password_totp_code": "HXDMVJECJJWSRB3HWIZR4IFUGFTMXBOZ",
+                "website_password_totp_digits": 6,
+                "website_password_totp_period": 30
             }, {
                 "id": generic_uuid,
                 "type": "credit_card",
