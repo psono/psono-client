d0a9cc646f9729db91d75e4fc37eb10feaf7def0
Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>
2025-04-10 07:41:53 +0200

    Upgraded node version to node v22
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>

--- var/prep-build.sh
+++ var/prep-build.sh
@@ -3,7 +3,7 @@ apt-get update && \
 apt-get install -y libfontconfig zip git apt-transport-https ca-certificates curl build-essential openssl gnupg && \
 mkdir -p /etc/apt/keyrings && \
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg  && \
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_16.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list  && \
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_22.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list  && \
 apt-get update && \
 apt-get install -y nodejs && \
 node --version && \
