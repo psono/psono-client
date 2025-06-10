493d806b1cbab11596f687c43cdd5134c49775ce
Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>
2025-06-10 11:09:29 +0200

    Fixing paths
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>

--- var/package-chrome-extension.sh
+++ var/package-chrome-extension.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-mkdir -p /builds/psono/psono-client/dist/chrome
-cd /builds/psono/psono-client/build/chrome/
-zip -r /builds/psono/psono-client/dist/chrome/psono.chrome.PW.zip *
-cd /builds/psono/psono-client/
+mkdir -p /builds/esaqa/psono/psono-client/dist/chrome
+cd /builds/esaqa/psono/psono-client/build/chrome/
+zip -r /builds/esaqa/psono/psono-client/dist/chrome/psono.chrome.PW.zip *
+cd /builds/esaqa/psono/psono-client/
