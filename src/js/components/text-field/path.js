b6d13e0f33e6415d4c2b87f6e9de5764d2a75ee4
Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>
2026-02-16 09:53:35 +0100

    Fix #148 Allow the user to choose a folder for password imports
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>

--- src/js/components/text-field/path.js
+++ src/js/components/text-field/path.js
@@ -2,19 +2,21 @@ import * as React from "react";
 import PropTypes from "prop-types";
 import InputLabel from "@mui/material/InputLabel";
 import FormControl from "@mui/material/FormControl";
+import FormHelperText from "@mui/material/FormHelperText";
 import InputAdornment from "@mui/material/InputAdornment";
 import { useTranslation } from "react-i18next";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import IconButton from "@mui/material/IconButton";
 import ClearIcon from "@mui/icons-material/Clear";
+import FolderOpenIcon from "@mui/icons-material/FolderOpen";
 
 const TextFieldPath = (props) => {
-    const { value, setPath, ...other } = props;
+    const { value, setPath, onOpenFolderSelect, helperText, required, ...other } = props;
     const { t } = useTranslation();
 
     return (
         <FormControl {...other}>
-            <InputLabel shrink htmlFor="component-simple">
+            <InputLabel shrink required={required} htmlFor="component-simple">
                 {t("PATH")}
             </InputLabel>
             <OutlinedInput
@@ -44,19 +46,31 @@ const TextFieldPath = (props) => {
                     ),
                 }}
                 endAdornment={
-                    value.length > 0 && (
+                    (onOpenFolderSelect || value.length > 0) && (
                         <InputAdornment position="end">
-                            <IconButton
-                                aria-label="clear path"
-                                onClick={() => setPath([])}
-                                edge="end"
-                                size="large">
-                                <ClearIcon fontSize="small" />
-                            </IconButton>
+                            {onOpenFolderSelect && (
+                                <IconButton
+                                    aria-label="select folder"
+                                    onClick={onOpenFolderSelect}
+                                    edge="end"
+                                    size="large">
+                                    <FolderOpenIcon fontSize="small" />
+                                </IconButton>
+                            )}
+                            {value.length > 0 && (
+                                <IconButton
+                                    aria-label="clear path"
+                                    onClick={() => setPath([])}
+                                    edge="end"
+                                    size="large">
+                                    <ClearIcon fontSize="small" />
+                                </IconButton>
+                            )}
                         </InputAdornment>
                     )
                 }
             />
+            {helperText && <FormHelperText>{helperText}</FormHelperText>}
         </FormControl>
     );
 };
@@ -64,6 +78,9 @@ const TextFieldPath = (props) => {
 TextFieldPath.propTypes = {
     value: PropTypes.array.isRequired,
     setPath: PropTypes.func.isRequired,
+    onOpenFolderSelect: PropTypes.func,
+    helperText: PropTypes.string,
+    required: PropTypes.bool,
 };
 
 export default TextFieldPath;
